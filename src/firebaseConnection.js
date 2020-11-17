21e74f8baa0896ecc5464b2d8dd7c0a870e5b847
Victor Manoel <victormanoel7895123@hotmail.com>
2020-11-17 15:25:57 -0300

    App created

---
+++ src/firebaseConnection.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyD9ROIbT2udnthZNFGWT4W4gXdsUV24L0w",
+    authDomain: "appfuncional-a2a3d.firebaseapp.com",
+    databaseURL: "https://appfuncional-a2a3d.firebaseio.com",
+    projectId: "appfuncional-a2a3d",
+    storageBucket: "appfuncional-a2a3d.appspot.com",
+    messagingSenderId: "1017700848683",
+    appId: "1:1017700848683:web:925d6522c143ad4315066e",
+    measurementId: "G-9WKY9ZC13K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
