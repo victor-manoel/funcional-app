4073a1bcc2df2cd89117e17bfc8642cad7656727
Weslley Borges <weslleynfs@gmail.com>
2021-02-10 22:12:21 -0300

    Search na tabela de historico Inclusão do diretório .vscode ao .gitignore

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,64 +1,59 @@
-import React,  {useState, useEffect, useContext} from 'react';
-import SearchList from '../../components/SearchList';
-import {View, Text} from 'react-native';
-import firebase from '../../services/firebaseConnection';
-import Feather from 'react-native-vector-icons/Feather';
-import {Container, AreaInput, Input, List} from './styles';
-import {AuthContext} from '../../contexts/auth';
+import React, { useState, useEffect, useContext } from "react";
+import SearchList from "../../components/SearchList";
+import { View, Text } from "react-native";
+import firebase from "../../services/firebaseConnection";
+import Feather from "react-native-vector-icons/Feather";
+import { Container, AreaInput, Input, List } from "./styles";
+import { AuthContext } from "../../contexts/auth";
 
-export default function Search(){
-    const [input, setInput] = useState('');
-    const [users, setUsers] = useState([]);
-    const {user:usuario} = useContext(AuthContext);
+export default function Search() {
+  const [input, setInput] = useState("");
+  const [users, setUsers] = useState([]);
+  const { user: usuario } = useContext(AuthContext);
 
-     useEffect(()=>{
-        let uid = usuario.uid;
-                
-        if(input ==='' || input === undefined){
-            setUsers([]);
-            return;
-        }
-        
-        let subscriber = firebase.database().ref('historico').child(uid).child(uid)
-        .orderByChild("nome")
-        .limitToFirst(5)
-        .on("value", function(snapshot) {
-            const listsUsers = [];
+  useEffect(() => {
+    let uid = usuario.uid;
 
-            snapshot.forEach( doc => {
-              listsUsers.push({
-               id: doc.id                                       
-            });
+    if (!input) {
+      setUsers([]);
+      return;
+    }
+
+    const historico = firebase.database().ref("historico");
+
+    historico
+      .child(uid)
+      .orderByChild("nome")
+      .on("value", function (snapshot) {
+        const listsUsers = [];
+
+        snapshot.forEach((historico) => {
+          historico.val().nome.includes(input.trim()) &&
+            listsUsers.push({ id: historico.key, ...historico.val() });
         });
+
         setUsers(listsUsers);
-        console.log(listsUsers);
-    })
+      });
+  }, [input]);
 
-    }, [input]);
+  return (
+    <Container>
+      <AreaInput>
+        <Feather name="search" color="#e52246" size={20} />
+        <Input
+          placeholder="Buscar"
+          placeholderTextColor="#353840"
+          value={input}
+          onChangeText={(text) => setInput(text)}
+        />
+      </AreaInput>
 
-    
-    return(
-        <Container>
-            <AreaInput>
-                <Feather
-                name="search"
-                color="#e52246"
-                size={20}
-                />
-                <Input
-                placeholder="Buscar"
-                placeholderTextColor="#353840"
-                value={input}
-                onChangeText={(text)=> setInput(text)}
-                />
-            </AreaInput>
-
-            <List
-                showsVerticalScrollIndicator={false}
-                data={users}
-                keyExtractor={(item) => item.id}
-                renderItem={({item}) => <SearchList data={item}/>}
-            />
-        </Container>
-    );
+      <List
+        showsVerticalScrollIndicator={false}
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <SearchList data={item} />}
+      />
+    </Container>
+  );
 }
