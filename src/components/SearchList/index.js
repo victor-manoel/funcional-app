a49e3c4c736a507cf968dbdc2f2e7114109dabb5
Weslley Borges <weslleynfs@gmail.com>
2021-02-11 03:42:56 -0300

    Exposiçao da prop OnPress do componente que representa o resultado da busca

--- src/components/SearchList/index.js
+++ src/components/SearchList/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Container, Name } from './styles';
+import React from "react";
+import { View, Text } from "react-native";
+import { Container, Name } from "./styles";
 
-export default function SearchList({data}) {
- return (
-   <Container onPress={() => alert('clicou')}>
-     <Name> {data.nome}</Name>
-   </Container>
+export default function SearchList({ data, onPress }) {
+  return (
+    <Container onPress={onPress}>
+      <Name> {data.nome}</Name>
+    </Container>
   );
 }
